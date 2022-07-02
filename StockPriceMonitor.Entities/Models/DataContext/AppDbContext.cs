f343228f96f402eb351d2add6b0246948ae32334
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2022-07-02 12:02:32 +0530

    Developments related to the DbContext

---
+++ StockPriceMonitor.Entities/Models/DataContext/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockPriceMonitor.Entities.Models.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<PriceSource> PriceSources { get; set; }
+    }
+}
---
+++ StockPriceMonitor.Repository.Pattern/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockPriceMonitor.Repository.Pattern
+{
+    public abstract class Entity
+    {
+        public bool IsDeleted { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+        [Required]
+        public string CreatedBy { get; set; }
+
+        public DateTime? DateLastUpdated { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
